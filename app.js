402efaf8d91852d2262881590c7917479ca7cc48
MunkhbayarMTJSC <munkhbayar.pch@gmail.com>
2023-04-01 21:16:46 +0800

    hereglegchiin oruulsan utgiig awdag blno

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
 //Дэлгэцтэй ажиллах контроллер
 
-var uiController = (function () {})();
+var uiController = (function () {
+  var DOMstring = {
+    inputType: ".add__type",
+    inputDesc: ".add__description",
+    inputValue: ".add__value",
+    addBtn: ".add__btn",
+  };
+  return {
+    getInput: function () {
+      return {
+        type: document.querySelector(DOMstring.inputType).value,
+        description: document.querySelector(DOMstring.inputDesc).value,
+        value: document.querySelector(DOMstring.inputValue).value,
+      };
+    },
+    getDomStrings: function () {
+      return DOMstring;
+    },
+  };
+})();
 
 //Санхүүтэй ажиллах контроллер
 
@@ -9,14 +28,16 @@ var financeController = (function () {})();
 //Программын холгогч контроллер
 
 var appController = (function (uiController, financeController) {
+  var DOM = uiController.getDomStrings();
   var ctrlAddItem = function () {
     // Оруулах өгөгдөлийг дэлгэцнээс олж авна.
+    console.log(uiController.getInput());
     // Олж авсан өгөгдөлүүдээ санхүүгийн контроллероор дамжуулж тэнд хадгална.
     // Олж авсан өгөгдөлүүдээ вэб дээрээ тохирох хэсэгт нь гаргана.
     // Төсвийг тооцоолно.
     // Эцсийн үлдэгдэл тооцоог дэлгэцэнд гаргана.
   };
-  document.querySelector(".add__btn").addEventListener("click", function () {
+  document.querySelector(DOM.addBtn).addEventListener("click", function () {
     ctrlAddItem();
   });
   document.addEventListener("keypress", function (event) {
